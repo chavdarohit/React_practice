ece6811ce491681c78d5109173db0de39b447ef3
Rohit chavda <rohitchavda449@gmail.com>
2024-02-28 19:24:11 +0530

    understand about the JSX & Functional Component

--- practise1/App.js
+++ practise1/App.js
@@ -1,10 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement(
   "h1",
   {
     id: "title",
+    key: "h1",
   },
-  "hey there this is element from the React"
+  "hey there this is element "
 );
 
+const container = React.createElement(
+  "div",
+  {
+    id: "inside-root",
+  },
+  [heading]
+);
+
+const HeaderFunctional = () => <h1>hii there this is functional component</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeaderFunctional />);
